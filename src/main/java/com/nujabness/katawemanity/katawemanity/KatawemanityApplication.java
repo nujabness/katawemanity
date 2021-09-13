d6d4e1fab5e9e5ce5b769786311c8faa609ef2bb
nujabness <m.elassouri@gmail.com>
2021-09-14 00:51:50 +0200

    Init

---
+++ src/main/java/com/nujabness/katawemanity/katawemanity/KatawemanityApplication.java
@@ -0,0 +1,13 @@
+package com.nujabness.katawemanity.katawemanity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatawemanityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatawemanityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nujabness/katawemanity/katawemanity/KatawemanityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nujabness.katawemanity.katawemanity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatawemanityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
