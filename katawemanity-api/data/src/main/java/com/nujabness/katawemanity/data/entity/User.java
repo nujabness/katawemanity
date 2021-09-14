cbbd260f0123860963247615c1c562ead0996259
nujabness <m.elassouri@gmail.com>
2021-09-14 15:58:28 +0200

    Init User

--- katawemanity-api/data/src/main/java/com/nujabness/katawemanity/data/entity/User.java
+++ katawemanity-api/data/src/main/java/com/nujabness/katawemanity/data/entity/User.java
@@ -1,7 +1,10 @@
 package com.nujabness.katawemanity.data.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
 @Table(name = "User")
 public class User {
@@ -23,43 +26,4 @@ public class User {
   @Column(name = "password")
   private String password;
 
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getNom() {
-    return nom;
-  }
-
-  public void setNom(String nom) {
-    this.nom = nom;
-  }
-
-  public String getPrenom() {
-    return prenom;
-  }
-
-  public void setPrenom(String prenom) {
-    this.prenom = prenom;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
 }
